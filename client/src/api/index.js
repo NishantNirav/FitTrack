167db9cd86907a15e2cb9eb718f4312d14f4a898
NishantNirav <nishantnirava@gmail.com>
2026-08-08 14:58:51 +0530

    Add vercel.json for frontend routing

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://fitness-tracker-server-rust.vercel.app/cls",
+  baseURL: "https://fitness-tracker-server-rust.vercel.app/",
   // baseURL: "http://localhost:8080/api/",
 });
 
