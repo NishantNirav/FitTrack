7e50c3c2f6df4f99d2a514cd42fad2bad7496bd9
NishantNirav <nishantnirava@gmail.com>
2026-08-08 14:42:06 +0530

    Fix vercel deployment setup for front end

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://fitnesstrack-vtv1.onrender.com/api/",
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://fitness-tracker-server-rust.vercel.app/cls",
+  // baseURL: "http://localhost:8080/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
